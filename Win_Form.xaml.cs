9af22b2920308cd4476e37af966ae7b90630ba06
fishman123456 <den9152@yandex.ru>
2023-03-29 22:50:54 +0700

    2
    2

--- Win_Form.xaml.cs
+++ Win_Form.xaml.cs
@@ -27,5 +27,15 @@ namespace autocad_wf_layer_add_29_03_2023
             data = _data;
           this.Window_1.DataContext = data;
         }
+
+        private void button_cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_ok_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
