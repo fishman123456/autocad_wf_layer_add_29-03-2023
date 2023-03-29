b7180738336ea958bd8c1f0c11e2aef97862ff6f
fishman123456 <den9152@yandex.ru>
2023-03-29 22:46:59 +0700

    22:46 спать нодо

--- Layer_Data.cs
+++ Layer_Data.cs
@@ -20,7 +20,7 @@ namespace autocad_wf_layer_add_29_03_2023
         {
             get
             {
-                return string.IsNullOrEmpty(Name);
+                return !string.IsNullOrEmpty(Name);
             }
         }
     }
